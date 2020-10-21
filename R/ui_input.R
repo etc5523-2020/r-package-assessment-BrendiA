0ba701c279158bc0a1cac96d01b9324f3a197779
BrendiA <ang0004@student.monash.edu>
2020-10-22 01:36:32 +1100

    Made Minor changes to function

--- R/ui_input.R
+++ R/ui_input.R
@@ -1,31 +1,27 @@
-#' Creates user interface input for Shiny app
+#' Creates reactive input for Shiny app
 #'
 #' @param data A data frame
 #' @param input_type Character string specifying type of input, "date" or "state"
 #' @param input_id A string specifying the label Id
-#' @param input_label A string specifying the label name for the reactive input
 #'
 #' @return
 #' Creates a reactive slider/picker input for Shiny App
-#'
-#' @examples
-#' ui_input("date", "choose_dates", us_tests_map, "Choose a Date")
 #'
 #' @importFrom shiny sliderInput
 #' @importFrom shinyWidgets pickerInput
 #'
 #' @export
 
-ui_input <- function(data, input_type, input_id, input_label) {
+ui_input <- function(input_type, input_id, data){
   if(input_type == "date"){
     sliderInput(inputId = input_id,
-                label = input_label,
+                label = "Choose a Date",
                 min = min(data$date),
                 max = max(data$date),
                 value = max(data$date))
   } else if(input_type == "state"){
     pickerInput(inputId = input_id,
-                label = input_label,
+                label = "Choose state(s)",
                 choices = sort(unique(data$state)),
                 selected = unique(data$state),
                 options = list(`actions-box` = TRUE),
