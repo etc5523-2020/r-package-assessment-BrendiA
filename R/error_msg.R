456ef15b43b0409412369673d20a7a39f61137b7
BrendiA <ang0004@student.monash.edu>
2020-10-23 17:50:29 +1100

    Added new parameter for error_msg function

--- R/error_msg.R
+++ R/error_msg.R
@@ -1,10 +1,9 @@
-
-
 #' Creates error message if Shiny App input is empty
 #'
 #' @param input Shiny user-interface input
+#' @param plot_type "bar" or "map"
 #'
-#' @return Error message if condition(s) are not met
+#' @return Error message if condition(s) are not met in the Shiny App
 #'
 #' @export
 #'
@@ -12,7 +11,9 @@
 error_msg <- function(input, plot_type){
   if(plot_type == "map") {
   shiny::validate(shiny::need(input != "", "Hello there, be sure to add a state or you'd run into an error"))
-  } else if(plot_type == "bar") {
+  }
+
+  else if(plot_type == "bar") {
     shiny::validate(shiny::need(input != "", ""))
     }
 }
